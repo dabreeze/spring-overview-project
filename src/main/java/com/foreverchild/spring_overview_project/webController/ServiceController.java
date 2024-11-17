4020861cb0f8d28213dfa66cd7bc274e01ffeada
foreverchild <harrisonogbonnaya@gmail.com>
2024-11-17 16:47:44 +0100

    overview

--- src/main/java/com/foreverchild/spring_overview_project/webController/ServiceController.java
+++ src/main/java/com/foreverchild/spring_overview_project/webController/ServiceController.java
@@ -1,13 +1,25 @@
 package com.foreverchild.spring_overview_project.webController;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServiceController {
 
-    @GetMapping("/first-endpoint")
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/home")
     public String getFirstEndpoint (){
         return "This is the first endpoint";
+    }
+
+    @GetMapping("/teaminfo")
+    public String teaemInfo(){
+        return "Coach: "+ coachName+"\n"+ " team name: "+teamName;
+
     }
 }
